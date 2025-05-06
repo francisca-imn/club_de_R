e243fb0f3707853a46c213b565fa6fc62fa96609
Francisca Muñoz Narbona <fmnarbona@uc.cl>
2025-05-06 14:29:46 -0400

    R for data science, introduccion

---
+++ codigo/02_intro_R_for_data_science.R
@@ -0,0 +1,18 @@
+#R for Data Science
+#https://github.com/hadley/r4ds
+#Introducción
+
+#Data: Import > tidy > transform > visualize > model > communicate
+
+library(tidyverse)
+# Tidyverse: dplyr, forcats, ggplot2, lubridate, purrr, readr, stringr, tibble, tidyr
+
+install.packages(c("arrow", "babynames", "curl", "duckdb", "gapminder", 
+    "ggrepel", "ggridges", "ggthemes", "hexbin", "janitor", "Lahman", 
+    "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins", 
+    "repurrrsive", "tidymodels", "writexl"))
+
+# ggplot2 ----
+
+library(palmerpenguins) #contiene bd penguins
+library(ggthemes)
